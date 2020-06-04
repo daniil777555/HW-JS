074299804f24c4320c1527e1645f1ffcdcd48e2d
Daniil <daniil2243@mail.ru>
2020-06-04 14:15:27 +0300

    HW 7

---
+++ src/Check.js
@@ -0,0 +1,115 @@
+"use strict";
+
+/*class Check {
+    constructor (){
+        this.fildElements = document.querySelectorAll(".fild-item");
+    }
+
+     checkWin(){ 
+        //Проверка по горизонтале для игрока с Х
+        if(document.querySelector('.fild-item0').innerHTML == "X" &&
+        document.querySelector('.fild-item1').innerHTML == "X" &&
+        document.querySelector('.fild-item2').innerHTML == "X"){
+            alert("Первый игрок победил")
+        }
+
+        if(document.querySelector('.fild-item3').innerHTML == "X" &&
+        document.querySelector('.fild-item4').innerHTML == "X" &&
+        document.querySelector('.fild-item5').innerHTML == "X"){
+            alert("Первый игрок победил")
+        }
+
+        if(document.querySelector('.fild-item6').innerHTML == "X" &&
+        document.querySelector('.fild-item7').innerHTML == "X" &&
+        document.querySelector('.fild-item8').innerHTML == "X"){
+            alert("Первый игрок победил")
+        }
+
+
+        //Проверка по горизонтале для игрока с 0
+        if(document.querySelector('.fild-item0').innerHTML == "O" &&
+        document.querySelector('.fild-item1').innerHTML == "O" &&
+        document.querySelector('.fild-item2').innerHTML == "O"){
+            alert("Первый игрок победил")
+        }
+
+        if(document.querySelector('.fild-item3').innerHTML == "O" &&
+        document.querySelector('.fild-item4').innerHTML == "O" &&
+        document.querySelector('.fild-item5').innerHTML == "O"){
+            alert("Первый игрок победил")
+        }
+
+        if(document.querySelector('.fild-item6').innerHTML == "O" &&
+        document.querySelector('.fild-item7').innerHTML == "O" &&
+        document.querySelector('.fild-item8').innerHTML == "O"){
+            alert("Первый игрок победил")
+        }
+
+        //Проверка по вертикале для игрока с Х
+        if(document.querySelector('.fild-item0').innerHTML == "X" &&
+        document.querySelector('.fild-item3').innerHTML == "X" &&
+        document.querySelector('.fild-item6').innerHTML == "X"){
+            alert("Первый игрок победил")
+        }
+
+        if(document.querySelector('.fild-item1').innerHTML == "X" &&
+        document.querySelector('.fild-item4').innerHTML == "X" &&
+        document.querySelector('.fild-item7').innerHTML == "X"){
+            alert("Первый игрок победил")
+        }
+
+        if(document.querySelector('.fild-item2').innerHTML == "X" &&
+        document.querySelector('.fild-item5').innerHTML == "X" &&
+        document.querySelector('.fild-item8').innerHTML == "X"){
+            alert("Первый игрок победил")
+        }
+
+        //Проверка по вертикале для игрока с 0
+        if(document.querySelector('.fild-item0').innerHTML == "O" &&
+        document.querySelector('.fild-item3').innerHTML == "O" &&
+        document.querySelector('.fild-item6').innerHTML == "O"){
+            alert("Первый игрок победил")
+        }
+
+        if(document.querySelector('.fild-item1').innerHTML == "O" &&
+        document.querySelector('.fild-item4').innerHTML == "O" &&
+        document.querySelector('.fild-item7').innerHTML == "O"){
+            alert("Первый игрок победил")
+        }
+
+        if(document.querySelector('.fild-item2').innerHTML == "O" &&
+        document.querySelector('.fild-item5').innerHTML == "O" &&
+        document.querySelector('.fild-item8').innerHTML == "O"){
+            alert("Первый игрок победил")
+        }
+
+        //Проверка по диагонале для игрока с Х
+        if(document.querySelector('.fild-item0').innerHTML == "X" &&
+        document.querySelector('.fild-item4').innerHTML == "X" &&
+        document.querySelector('.fild-item8').innerHTML == "X"){
+            alert("Первый игрок победил")
+        }
+
+        if(document.querySelector('.fild-item2').innerHTML == "X" &&
+        document.querySelector('.fild-item4').innerHTML == "X" &&
+        document.querySelector('.fild-item6').innerHTML == "X"){
+            alert("Первый игрок победил")
+        }
+
+        //Проверка по диагонале для игрока с O
+        if(document.querySelector('.fild-item0').innerHTML == "O" &&
+        document.querySelector('.fild-item4').innerHTML == "O" &&
+        document.querySelector('.fild-item8').innerHTML == "O"){
+            alert("Первый игрок победил")
+        }
+
+        if(document.querySelector('.fild-item2').innerHTML == "O" &&
+        document.querySelector('.fild-item4').innerHTML == "O" &&
+        document.querySelector('.fild-item6').innerHTML == "O"){
+            alert("Первый игрок победил")
+        }
+    }
+
+
+
+}*/
